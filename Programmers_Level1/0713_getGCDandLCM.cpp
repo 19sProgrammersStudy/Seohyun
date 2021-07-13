4f6e5ebce6f3686d76c46e785b52b82f14ae2b86
bewisessong <bewisessong@gmail.com>
2021-07-13 23:22:18 +0900

    7/13(화) 문제풀이 : 최대공약수와 최소공배수

--- Programmers_Level1/0713_getGCDandLCM.cpp
+++ Programmers_Level1/0713_getGCDandLCM.cpp
@@ -1,38 +1,46 @@
 // Level 1. 최대공약수와 최소공배수
-// 
+// https://programmers.co.kr/learn/courses/30/lessons/12940
 
 #include <string>
 #include <vector>
 
 using namespace std;
 
-bool isGCD(int n, int m, int div) {
-    if(n % div == 0 && m % div == 0)
-        return true;
-    else
-        return false;
+bool isGCD(int n, int m, int div)
+{
+  if (n % div == 0 && m % div == 0)
+    return true;
+  else
+    return false;
 }
 
-vector<int> solution(int n, int m) {
-    vector<int> answer;
-    
-    if(n >= m) {
-        for(int idx=m; idx>=1; idx--) {
-            if(isGCD(n, m, idx)) {
-                answer.push_back(idx);
-                break;
-            }
-        }
+vector<int> solution(int n, int m)
+{
+  vector<int> answer;
+
+  if (n >= m)
+  {
+    for (int idx = m; idx >= 1; idx--)
+    {
+      if (isGCD(n, m, idx))
+      {
+        answer.push_back(idx);
+        break;
+      }
     }
-    else {
-        for(int idx=n; idx>=1; idx--) {
-            if(isGCD(n, m, idx)) {
-                answer.push_back(idx);
-                break;
-            }
-        }
+  }
+  else
+  {
+    for (int idx = n; idx >= 1; idx--)
+    {
+      if (isGCD(n, m, idx))
+      {
+        answer.push_back(idx);
+        break;
+      }
     }
-    
-    answer.push_back((n*m)/answer[0]);
-    return answer;
+  }
+
+  answer.push_back((n * m) / answer[0]);
+  return answer;
 }
