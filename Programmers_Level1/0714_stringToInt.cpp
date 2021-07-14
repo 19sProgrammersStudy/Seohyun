7cfcd8cd40c6b194ec519c862c08f5987fd4c4b1
bewisessong <bewisessong@gmail.com>
2021-07-14 21:52:00 +0900

    7/14(수) 문제풀이 : 문자열을 정수로 바꾸기

---
+++ Programmers_Level1/0714_stringToInt.cpp
@@ -0,0 +1,12 @@
+// Level 1. 문자열을 정수로 바꾸기
+// https://programmers.co.kr/learn/courses/30/lessons/12925
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string s)
+{
+  return stoi(s);
+}
