a9d40facc7dfbb2472162298449e3647934f4537
bewisessong <bewisessong@gmail.com>
2021-07-14 21:40:15 +0900

    7/14(수) 문제풀이 : 자릿수 더하기

---
+++ Programmers_Level1/0714_addDigit.cpp
@@ -0,0 +1,18 @@
+// Level 1. 자릿수 더하기
+// https://programmers.co.kr/learn/courses/30/lessons/12931
+
+#include <iostream>
+
+using namespace std;
+int solution(int n)
+{
+  int answer = 0;
+
+  while (n != 0)
+  {
+    answer += n % 10;
+    n /= 10;
+  }
+
+  return answer;
+}
