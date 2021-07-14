dc62677f2a4501bf499973f841b071fcda142604
bewisessong <bewisessong@gmail.com>
2021-07-14 22:06:29 +0900

    7/14(수) 문제풀이 : 서울에서 김서방 찾기

---
+++ Programmers_Level1/0714_kimInSeoul.cpp
@@ -0,0 +1,13 @@
+// Level 1. 서울에서 김서방 찾기
+// https://programmers.co.kr/learn/courses/30/lessons/12919
+
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+string solution(vector<string> seoul)
+{
+  return "김서방은 " + to_string(find(seoul.begin(), seoul.end(), "Kim") - seoul.begin()) + "에 있다";
+}
