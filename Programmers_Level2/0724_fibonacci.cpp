7d8c3b09b56c70c26688d9fe544b65c908ce140a
bewisessong <bewisessong@gmail.com>
2021-07-24 14:54:06 +0900

    7/24(금) 문제풀이 : 피보나치 수

---
+++ Programmers_Level2/0724_fibonacci.cpp
@@ -0,0 +1,25 @@
+// Level 2. 피보나치 수
+// https://programmers.co.kr/learn/courses/30/lessons/12945?language=cpp
+
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+#define MAX 100001
+#define DIV 1234567
+
+int solution(int n)
+{
+  int fibonacci[MAX] = {
+      0,
+  };
+
+  fibonacci[1] = 1;
+
+  for (int idx = 2; idx <= n; idx++)
+    fibonacci[idx] = (fibonacci[idx - 1] + fibonacci[idx - 2]) % DIV;
+
+  return fibonacci[n];
+}
